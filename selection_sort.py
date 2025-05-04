999e9bf26b48e4964225949d0794b5f476ebb460
SelmanEminKulfal <selmaneminkulfal@gmail.com>
2025-05-04 15:21:04 +0300

    selection sort algorithm updated...

--- selection_sort.py
+++ selection_sort.py
@@ -1,12 +1,11 @@
 n = int(input("How many numbers? : "))
 my_array = [int(input("Enter your number : ")) for _ in range(n)] 
 
-for i in range(n-1):
+for i in range(n):
     min_index = i
     for j in range(i+1, n):
         if my_array[j] < my_array[min_index]:
-            min_index = j
-    min_value = my_array.pop(min_index)
-    my_array.insert(i, min_value)
+            min_index = j   
+    my_array[i], my_array[min_index] = my_array[min_index], my_array[i]
 
-print("Sorted array:", my_array)   
+print("Sorted array:", my_array)  
