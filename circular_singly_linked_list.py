d6baf5c69e8abdcd13955b538246f258531ecbca
SelmanEminKulfal <selmaneminkulfal@gmail.com>
2025-05-04 18:16:32 +0300

    circular singly linked list added...

---
+++ circular_singly_linked_list.py
@@ -0,0 +1,27 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+    
+node1 = Node(int(input("Type first number : ")))
+node2 = Node(int(input("Type second number : ")))
+node3 = Node(int(input("Type third number : ")))
+node4 = Node(int(input("Type fourth number : ")))
+node5 = Node(int(input("Type fifth number : ")))
+
+node1.next = node2 # type: ignore
+node2.next = node3 # type: ignore
+node3.next = node4 # type: ignore
+node4.next = node5 # type: ignore
+node5.next = node1 # type: ignore
+
+currentNode = node1
+startNode = node1
+print(currentNode.data, end=" -> ") 
+currentNode = currentNode.next 
+
+while currentNode != startNode:
+    print(currentNode.data, end=" -> ") # type: ignore
+    currentNode = currentNode.next # type: ignore
+
+print("...")
